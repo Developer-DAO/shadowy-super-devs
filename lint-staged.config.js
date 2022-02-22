5e080defc77ebfb52f375775b7e9afac95a87c4d
Yusuf Kehinde Hussein <yusufkehinde11@gmail.com>
2022-02-22 18:07:24 +0100

    fix: lint stage

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   // Run ESLint on changes to JavaScript/TypeScript files
-  "**/*.(ts|js)?(x)": (filenames) => `yarn lint:fix . ${filenames.join(" ")}`,
+  // "**/*.(ts|js)?(x)": (filenames) => `yarn lint:fix . ${filenames.join(" ")}`,
+  "src/**/*.+(js|json|ts|tsx|jsx)": "eslint",
+  "src/**/*.{js,jsx,ts,tsx,json,css,scss,md}": "next lint --fix",
 };
